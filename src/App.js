d735ff5bebe95c096b0915b7be4f163b77f4e00d
João Rangel <joaohcrangel@gmail.com>
2020-11-24 20:02:29 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [resultado, defineResultado] = React.useState("");
+  const [nome, defineNome] = React.useState("Pedro Silva");
+  const [menos25, defineMenos25] = React.useState(true);
+  const [trabalhando, defineTrabalhando] = React.useState(false);
+  const [filhos, defineFilhos] = React.useState(false);
+
+  const primeiroNome = () => {
+    return nome.split(" ")[0];
+  };
+
+  const descricaoMenos25 = () => {
+    return menos25 ? `` : `não`;
+  };
+
+  const descricaoTrabalhando = () => {
+    return trabalhando
+      ? `felizmente está trabalhando`
+      : `infelizmente está desempregado`;
+  };
+
+  const descricaoFilhos = () => {
+    return filhos
+      ? `precisa cuidar dos filhos`
+      : `não precisa cuidar dos filhos`;
+  };
+
+  React.useEffect(() => {
+    defineResultado(
+      `${primeiroNome()} ${descricaoMenos25()} é jovem, ${descricaoTrabalhando()} e ${descricaoFilhos()}.`
+    );
+  }, [nome, menos25, trabalhando, filhos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <TextField
+          style={{ width: "100%" }}
+          label="Nome"
+          value={nome}
+          onChange={(event) => defineNome(event.target.value)}
+        />
+      </section>
+      <section>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={menos25}
+              onChange={(event) => defineMenos25(event.target.checked)}
+            />
+          }
+          label="Menos de 25 anos"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={trabalhando}
+              onChange={(event) => defineTrabalhando(event.target.checked)}
+            />
+          }
+          label="Trabalhando"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={filhos}
+              onChange={(event) => defineFilhos(event.target.checked)}
+            />
+          }
+          label="Tem filhos"
+        />
+      </section>
+      <section>{resultado}</section>
     </div>
   );
 }
-
-export default App;
